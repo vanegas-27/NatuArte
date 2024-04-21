f81e27df6af67a3a042fcff5127d52cc1b1b6b67
Davinson vanegas <davidsonvanegas27@gmail.com>
2024-04-20 20:33:30 -0500

    [UPDATE] se modifico la vista de los productos y se añadieron las nuevas fotos y videos

---
+++ src/content/flores.ts
@@ -0,0 +1,22 @@
+import { type post } from "./typos.ts";
+
+export const flores : post[] = [
+    {
+        id: 1,
+        img : "/img/01.webp",
+        price : 3000,
+        texto : "Flor de loto.",
+    },
+    {
+        id: 2,
+        img : "/img/02.webp",
+        price : 4300,
+        texto : "Flor rosa del caribe.",
+    },
+    {
+        id: 3,
+        img : "/img/03.webp",
+        price : 2340,
+        texto : "Flor rosa del norte.",
+    }
+]
---
+++ src/content/typos.ts
@@ -0,0 +1,12 @@
+
+type post = {
+    id: number;
+    img : string;
+    price : number;
+    texto : string;
+}
+
+
+export {
+    type post,
+};
